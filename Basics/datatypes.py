cce772117c581360b060d8d80147f2a148fb2b84
mitansh99 <mitanshpatel99@gmail.com>
2025-07-28 22:14:31 +0530

    Learning- Datatypes

---
+++ Basics/datatypes.py
@@ -0,0 +1,32 @@
+# python built in default data types are --------------------------
+
+# For text : str
+x = "Hello World!"
+
+# Numeric type 
+x = 20 # int 
+x = 3.14 # float 
+x = 1j # complex
+
+# sequence type 
+x = ["apple","banana", "cherry"] # list
+x = ("apple", "bamama","cherry") # tuple
+x = range(6) # range
+
+# mapping type
+x = {"name" : "mitansh", "age": 20} # dict
+
+# set type : ,frozenset
+x = {"apple","banana","watermalon"} # set
+x = frozenset({"apple","banana","cherry"}) #frozenset
+
+# boolean type 
+x = True # bool
+
+# binary type 
+x = b"Hello" # bytes
+x = bytearray(5) # bytearray
+x = memoryview(bytes(5)) # memoryview
+
+# none  type
+x = None # NoneType
