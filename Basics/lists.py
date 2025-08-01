3284be357f7801a7aea2ecb106f95bdc3e500aea
mitansh99 <mitanshpatel99@gmail.com>
2025-08-01 21:34:02 +0530

    Learning List

---
+++ Basics/lists.py
@@ -0,0 +1,31 @@
+# List ----------------------
+# list are use to store multipule value in a single variable
+
+# Built in datatypes
+
+thislist = ["apple" , "banana", "cherry"]
+print(thislist)
+
+# List items are orderd, changable, and allow duplicate val
+
+# items have indeies , access bu the [0] second [1]  ---> define order that will not gona change
+
+# list is mutable , add , change , remove items after creating list
+# allow duplicate because diff by indesies
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+
+# len() to get the number of the items list contains
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+# List can contain diffrent types of values
+
+# list type is a <class 'list'>
+print(type(thislist))
+
+# we can use list constructor to create a new list
+
+thislist = list(("apple", "banana", "cherry")) 
