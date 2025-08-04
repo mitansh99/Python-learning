6a672d5484ea7f7751fe9beeb1c159b4730d5302
mitansh99 <mitanshpatel99@gmail.com>
2025-08-04 21:47:21 +0530

    Learning - List & their Working

--- Basics/lists.py
+++ Basics/lists.py
@@ -30,4 +30,47 @@ print(type(thislist))
 
 thislist = list(("apple", "banana", "cherry")) 
 
-# streak
+# Access List items  ----------------------------------------
+
+print(thislist[1]) # First item have 0 index
+
+# Nagative indexing means start from the back 
+# -1 for the last element and so on 
+
+# Range of indexies -------------------------
+
+thislist[2:5] # always return new list 
+
+# srearch will be 2 (inc) bto 5(not inc)
+
+# [:4] for start from first and end to the 4 th element
+# [2:] start from 2 and all availabale elemetns
+
+if "apple" in thislist:  # To find iteam avaiable or not
+    print("yes it is") 
+
+# Change Item value
+
+thislist[1] = "guwawa" # replace the value with new 
+
+# Range of value change  
+thislist[1:3] = ["blackbarry","watermalon"]
+
+# If you insert more items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly
+
+
+# The length of the list will change when the number of items inserted does not match the number of items replaced.
+
+# If you insert less items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly:
+
+thislist[1:3] = ["watermelon"]
+
+# Insert() use to add the new value to the list whitout replacing exe values
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon") # insert(index, value)
+
+
+
+
+
