f5bf0e138ef7d57f6f678bd6f8e8a7a036ccad74
mitansh99 <mitanshpatel99@gmail.com>
2025-08-12 20:39:35 +0530

    Streak

--- Basics/tuples.py
+++ Basics/tuples.py
@@ -181,3 +181,5 @@ print(mytuple)
 # count() - Returns the number of times a specified value occurs in a tuple
 
 # index() - Searches the tuple for a specified value and returns the position of where it was found
+
+# Streak
