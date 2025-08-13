48d203311fda8bb902cafc34daef01207d128196
mitansh99 <mitanshpatel99@gmail.com>
2025-08-13 21:32:40 +0530

    Learning - sets

---
+++ Basics/stes.py
@@ -0,0 +1,22 @@
+# stets are used to store multipule values in single var
+
+# sets = unordered, unchangeable*, and unindexed
+thisset = {"apple", "banana", "cherry"}
+
+# Sets are unordered, so you cannot be sure in which order the items will appear.
+
+# Sets - do not allow duplicate values.
+
+# We can't remove the items from the set but we can remove items
+
+# true and 1 is equal in sets (SAME VALUE) FALSE == 0
+
+len(thisset) # get the length of the set
+
+type(thisset) # <class 'set'>
+
+# It is also possible to use the set() constructor to make a set.
+
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+
