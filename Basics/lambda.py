649f3f818eb1090a408fabd5cdc621c91d66cd31
mitansh99 <mitanshpatel99@gmail.com>
2025-09-08 21:00:12 +0530

    Learning Lambda

---
+++ Basics/lambda.py
@@ -0,0 +1,11 @@
+# Lambda - is a small anonymous function 
+
+# can take any number of args but only have one exp
+# syntex - lambda arguments : expression
+
+x = lambda a : a + 10 # add to number
+print(x(5))
+
+x = lambda a, b : a * b # multipication and retuen
+print(x(5, 6))
+
