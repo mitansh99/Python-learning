4782f389aa60c63583cb24b03bdc63856ad3a0fd
mitansh99 <mitanshpatel99@gmail.com>
2025-11-06 05:55:54 +0530

    Learned inbuilt math functions and math module

---
+++ Basics/math.py
@@ -0,0 +1,24 @@
+# python have a built in math function
+
+# min()and max() function can be used to find the lowest and highest value in an iterable
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+# function return the absolute (positive) value of the specified number
+x = abs(-7)
+
+
+# pow() function return the value of x to the power of y (xy)
+x = pow(4, 3) # 4*4*4 = 64
+
+# Python also has a built-in module called math, which extends the list of mathematical functions.
+import math
+
+math.sqrt(64) # return the square root of a number
+
+math.ceil(1.4) # return the upper integer value i.e 2
+
+math.floor(1.4) # return the lower integer value i.e 1
+
+math.pi # return the value of pi 3.141592653589793
