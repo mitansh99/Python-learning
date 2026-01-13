e972781f62035bb1b07b3ae1399ab362469e947e
mitansh99 <mitanshpatel99@gmail.com>
2026-01-13 22:15:51 +0530

    numpy

---
+++ Numpy/basics.py
@@ -0,0 +1,11 @@
+# Numpy is a python lib used for working with arrays.
+# it has also fun for working in domain for linear algebra, fourier transform, and matices
+# NumPy stands for Numerical python
+
+# NumPy -> python have list == arrays but they are slow to process
+
+# NumPy is 50x fister then traditional python list
+
+# object in NumPy is called ndarray 
+
+# store at one continues place 
