3e7eca5cf70a5fc508a698bf165002e25f5c6787
mitansh99 <mitanshpatel99@gmail.com>
2026-01-16 21:49:44 +0530

    numpy setup and array

---
+++ Numpy/array.py
@@ -0,0 +1,10 @@
+import numpy as np
+# Create numpy array
+array = np.array([1, 2, 3, 4])
+
+print(array)
+print(type(array)) # Output: <class 'numpy.ndarray'>
+
+array *= 2
+
+print(array) # Output: [2 4 6 8]
